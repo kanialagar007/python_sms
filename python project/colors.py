78ed269acb87e942196863adf27412c9f7dedfa1
kanialagar007 <96720604+kanialagar007@users.noreply.github.com>
2023-05-13 19:12:05 +0530

    Add files via upload

---
+++ python project/colors.py
@@ -0,0 +1,9 @@
+color_1 = "skyblue"
+color_2 = "skyblue"
+color_3 = "black"
+color_4 = "white"
+color_5 = "pink"
+
+
+font_1 = "times new roman"
+font_2 = "helvetica"
---
+++ python project/database.py
@@ -0,0 +1,5 @@
+# User database
+host = 'localhost'
+user = 'root'
+password = 'password1'
+database = 'aksms'
